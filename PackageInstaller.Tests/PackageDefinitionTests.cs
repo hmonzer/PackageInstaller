aeff5135192d33ad9c62768faec595826618f027
hmonzer <hsen.monzer@gmail.com>
2015-06-27 09:34:20 +0300

    Solution creation + class test for PackageDefinition
    - Initial solution submission
    - Test for Package Definition

---
+++ PackageInstaller.Tests/PackageDefinitionTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PackageInstaller.Tests
+{
+    [TestFixture]
+    public class PackageDefinitionTests
+    {
+        [Test, TestCaseSource("packageTestData")]
+        public void given_a_string_constructs_a_correct_PackageNode(string input, string package,
+            List<string> dependencies)
+        {
+            PackageDefinition packageDef = new PackageDefinition(input);
+            Assert.AreEqual(packageDef.Package, package);
+            Assert.IsFalse(packageDef.Dependencies.Except(dependencies).Any());
+
+        }
+
+        public IEnumerable<TestCaseData> packageTestData
+        {
+            get
+            {
+                Collection<TestCaseData> testCases = new Collection<TestCaseData>();
+
+                TestCaseData noDepdencyCase = new TestCaseData("A", "A", new List<string>());
+                testCases.Add(noDepdencyCase);
+                TestCaseData singleDepdencyCase = new TestCaseData("A: B", "A", new List<string>() {"B"});
+                testCases.Add(singleDepdencyCase);
+                TestCaseData multipleDependencyCase = new TestCaseData("A: B, C, D", "A",
+                    new List<string>() {"B", "C", "D"});
+                testCases.Add(multipleDependencyCase);
+
+                return testCases;
+            }
+        }
+    }
+}
---
+++ PackageInstaller/PackageDefinition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageInstaller
+{
+    /// <summary>
+    /// Entity used to consolidate logic for parsing input as a string A: B, C, D and map it to a Package with a list of Dependencies
+    /// </summary>
+    public class PackageDefinition
+    {
+        public string Package { get; set; }
+
+        private List<string> _dependencies { get; set; }
+
+        public IEnumerable<string> Dependencies
+        {
+            get { return _dependencies.AsEnumerable(); }
+        }
+
+        public PackageDefinition(string packageDefinition)
+        {
+            
+        }
+    }
+}
