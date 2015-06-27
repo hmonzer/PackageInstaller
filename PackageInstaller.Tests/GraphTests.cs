509425da558bf1895e2740e129922741649f03ba
hmonzer <hsen.monzer@gmail.com>
2015-06-27 10:37:51 +0300

    Adding unit test for Adding Node to the graph
    Function still not implements

--- PackageInstaller.Tests/GraphTests.cs
+++ PackageInstaller.Tests/GraphTests.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace PackageInstaller.Tests
 {
     public class GraphTests
     {
-
+        [Test]
+        public void AddNode_adds_node_into_list()
+        {
+            Graph graph = new Graph();
+            PackageDefinition definition = new PackageDefinition("A");
+            graph.AddPackageDefinition(definition);
+            Assert.IsTrue(graph.Contains(definition.Package));
+        }
     }
 }
