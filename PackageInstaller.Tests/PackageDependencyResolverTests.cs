8df2855b58b1229ee8499cc2178b0194cfc659ea
hmonzer <hsen.monzer@gmail.com>
2015-06-27 12:25:18 +0300

    Add Acceptance Test

---
+++ PackageInstaller.Tests/PackageDependencyResolverTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PackageInstaller.Tests
+{
+    [TestFixture]
+    public class PackageDependencyResolverTests
+    {
+        [Test, TestCaseSource("AcceptanceTestCases")]
+        public void Acceptance_test(string[] packages, string output)
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                PackageDependencyResolver.Main(packages);
+                Assert.AreEqual("", sw.ToString());
+            }
+        }
+
+        public IEnumerable<TestCaseData> AcceptanceTestCases
+        {
+            get
+            {
+                Collection<TestCaseData> cases = new Collection<TestCaseData>();
+                string[] validPackages = new[]
+                {
+                    "KittenService:", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService",
+                    "Fraudstream: Leetmeme", "Ice:"
+                };
+                string validOutput = "KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream";
+                TestCaseData validData = new TestCaseData(validPackages, validOutput);
+                cases.Add(validData);
+
+                string[] InvalidPackages = new[]
+                {
+                    "KittenService:", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService",
+                    "Fraudstream:", "Ice: Leetmeme"
+                };
+                TestCaseData invalidData = new TestCaseData(InvalidPackages, null).Throws(typeof(InvalidOperationException));
+                cases.Add(invalidData);
+
+                return cases;
+            }
+        } 
+    }
+}
